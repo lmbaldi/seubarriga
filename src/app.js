62e83062a45c9fc6a8f58fe0c5fcfa44d90062c2
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-02-22 15:59:35 -0300

    deve listar todos os usuarios

--- src/app.js
+++ src/app.js
@@ -4,5 +4,12 @@ app.get('/', (req, res) => {
     res.status(200).send();
 });
 
+app.get('/users', (req,res) => {
+    const users = [
+        {name: 'Bianca', email: 'bianca@gmail.com'},
+    ];
+    res.status(200).json(users);
+});
+
 // eslint-disable-next-line no-undef
 module.exports = app;
